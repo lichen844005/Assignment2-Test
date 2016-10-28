bf91f9121445e504c509bb2a911125e16d56c860
chen li <chen li>
2016-10-27 17:42:25 -0700

    better movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Movement : MonoBehaviour {
     public float speed = 5f;
-    public Camera camera;
+    public CharacterController controller;
+    private Vector3 moveDirection = Vector3.zero;
     // Use this for initialization
     void Start () {
 	
@@ -13,27 +14,31 @@ public class Movement : MonoBehaviour {
 	void Update () {
         if (Input.GetKey(KeyCode.A))
         {
-            transform.Rotate(new Vector3(0, speed, 0));
+            transform.Rotate(new Vector3(0, -speed, 0));
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(new Vector3(0, -speed, 0));
+            transform.Rotate(new Vector3(0, speed, 0));
         }
         if (Input.GetKey(KeyCode.Q))
         {
-            transform.Translate(Vector3.left * speed * Time.deltaTime);
+            transform.Translate(Vector3.left * speed);
         }
         if (Input.GetKeyDown(KeyCode.E))
         {
-            transform.Translate(Vector3.left * -speed * Time.deltaTime);
+            transform.Translate(Vector3.left * -speed);
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            moveDirection = transform.TransformDirection(Vector3.forward) * speed;
+            //transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.Translate(Vector3.forward * -speed * Time.deltaTime);
+            moveDirection = transform.TransformDirection(Vector3.forward) * -speed;
+            //transform.Translate(Vector3.forward * -speed * Time.deltaTime);
         }
+        controller.Move(moveDirection * Time.deltaTime);
+        moveDirection = Vector3.zero;
     }
 }
