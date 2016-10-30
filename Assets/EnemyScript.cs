def0729aa317c9c7fcf855f2eedfa3597dcab337
John Janzen <joalja2002@msn.com>
2016-10-29 22:21:05 -0700

    Enemy with slight random path finding

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+struct Node
+{
+    List<Node> nodes;
+    Vector3 pos;
+}
+
+public class EnemyScript : MonoBehaviour {
+
+    public Vector3 startPos, moveDirection, currentPos;
+    public CharacterController controller;
+    private float speed = 1f;
+    private int direction;
+    private int[] pathways = new int[4];
+    private Node[,] tree = new Node[10,10];
+
+	// Use this for initialization
+	void Start () {
+        startPos = currentPos = transform.position;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+
+            }
+        }
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 fwd = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, fwd, 0.5f))
+        {
+            WallHitDirectionChange();
+        }
+        moveDirection = transform.TransformDirection(Vector3.forward) * speed;
+        controller.Move(moveDirection * Time.deltaTime);
+    }
+
+    void WallHitDirectionChange()
+    {
+
+        Vector3 fwd;
+        int greatest = -1;
+        for (int j = 0; j < 4; j++)
+        {
+            transform.Rotate(0, 90f, 0);
+            fwd = transform.TransformDirection(Vector3.forward);
+            if (!Physics.Raycast(transform.position, fwd, 0.5f))
+            {
+                transform.position += transform.forward;
+                for (int i = 0; i < 4; i++)
+                {
+                    transform.Rotate(0, 90f, 0);
+                    fwd = transform.TransformDirection(Vector3.forward);
+                    if (!Physics.Raycast(transform.position, fwd, 0.5f))
+                    {
+                        pathways[j]++;
+                    }
+                }
+                transform.position -= transform.forward;
+            }
+
+        }
+
+        for (int i = 0; i < pathways.Length; i++)
+        {
+            if (greatest < pathways[i])
+            {
+                direction = i;
+                greatest = pathways[i];
+            }
+            else if (greatest == pathways[i])
+            {
+                System.Random random = new System.Random();
+                if (random.Next(0, 100) <= 50)
+                {
+                    direction = i;
+                }
+            }
+        }
+        pathways = new int[4];
+        transform.Rotate(new Vector3(0, (direction + 1) * 90f, 0));
+    }
+}
