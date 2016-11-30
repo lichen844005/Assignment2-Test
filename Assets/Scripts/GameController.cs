1bd82253ea9e0f41c02bd19e37ab373dd9e3b8cc
chen li <chen li>
2016-11-29 18:54:29 -0800

    update

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class GameController : MonoBehaviour {
+
+    public static GameController gameController;
+
+    public float playerPositionX;
+    public float playerPositionY;
+    public float playerPositionZ;
+
+    void Awake()
+    {
+        if (gameController == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gameController = this;
+
+        }
+        else if (gameController != this)
+
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Save()
+
+    {
+
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/playerInfo.dat");
+
+
+        PlayerData data = new PlayerData();
+        data.playerPosX = playerPositionX;
+        data.playerPosY = playerPositionY;
+        data.playerPosZ = playerPositionZ;
+
+        bf.Serialize(file, data);
+        file.Close();
+    }
+
+    public void Load ()
+    {
+        if (File.Exists(Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/playerInfo.dat", FileMode.Open);
+
+            PlayerData data = (PlayerData)bf.Deserialize(file);
+            file.Close();
+
+            playerPositionX = data.playerPosX;
+            playerPositionY = data.playerPosY;
+            playerPositionZ = data.playerPosZ;
+        }
+    }
+
+    public void Delete()
+    {
+        if (File.Exists (Application.persistentDataPath + "/playerInfo.dat"))
+        {
+            File.Delete(Application.persistentDataPath + "/playerInfo.dat");
+        }
+    }
+
+}
+
+[Serializable]
+class PlayerData
+{
+    public float playerPosX;
+    public float playerPosY;
+    public float playerPosZ;
+
+}
---
+++ Assets/Scripts/PlayerSave.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSave : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown (KeyCode.X))
+        {
+            GameController.gameController.Delete();
+        }
+
+        if (Input.GetButtonDown ("Save"))
+        {
+            GameController.gameController.playerPositionX = transform.position.x;
+            GameController.gameController.playerPositionY = transform.position.y;
+            GameController.gameController.playerPositionZ = transform.position.z;
+        }
+
+        if (Input.GetButtonDown("Load"))
+        {
+            GameController.gameController.Load();
+
+            transform.position = new Vector3
+
+                (
+                GameController.gameController.playerPositionX,
+                GameController.gameController.playerPositionY,
+                GameController.gameController.playerPositionZ
+                );
+        }
+
+
+    }
+}
